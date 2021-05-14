1af4a81920fefd720e1e44215a96c8cb38280d70
mjanks <mjanks10@gmail.com>
2021-05-14 16:07:44 -0400

    Added UserList and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Redirect to="/" />
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [
+    {
+      id: "u1",
+      name: "Mike Janks",
+      image:
+        "https://www.cleveland.com/resizer/zKqVOuz7Vxscc_2Adj0qc2_T3lc=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/I5IYVJ4H5RCYDF7SZF2BDUGOOI.jpeg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
