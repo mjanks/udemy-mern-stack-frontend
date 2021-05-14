38848a17b052f8784060937cd12b910ae0fbd122
mjanks <mjanks10@gmail.com>
2021-05-14 11:48:43 -0400

    Added a route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+import Users from './user/pages/Users';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Redirect to="/" />
+    </Router>
+  );
 }
 
 export default App;
